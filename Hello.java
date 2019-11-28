3f00a04f4d28e8bc9ca6f1646e0f4d1b72cf7265
dmdwns <dmdwns0220@gmail.com>
2019-11-28 19:48:03 +0900

    깃?
    됨?

---
+++ Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello world~~~"); //문자열 : String
+		System.out.println("Hello Java!");
+		System.out.println(100 + 50);
+		System.out.println("100" + 50);
+		System.out.println("Hello" + 50);
+		System.out.println(100 / 50);
+		System.out.println(100 * 50);
+		System.out.println(100 - 50);
+		System.out.println(100 % 50);
+		System.out.println(100 % 95); // 나눈 나머지 : %
+	}
+}
